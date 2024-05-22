9df925ceefa59d14c5ace5e8fed809987450e0d5
Yaakov Meir <ymeir02@gmail.com>
2024-05-22 11:02:57 -0300

    Close #2: configurando rotas Públicas

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,11 @@
-
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import RoutesApp from "./routes";
 
 function App() {
   return (
-    <div >
-     <h1>teste</h1>
-    </div>
+    <BrowserRouter>
+      <RoutesApp />
+    </BrowserRouter>
   );
 }
 
